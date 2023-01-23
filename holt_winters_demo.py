ec46af73291da2ee2068928fdcde196395142229
Miguel Burgos <mburgtech@gmail.com>
2023-01-23 19:28:51 +0100

    initial comit with some structure

---
+++ holt_winters_demo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 18:46:49 2023
+
+@author: Miguel Burgos
+"""
+
+import pandas as pd
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+
+# Exploring the dataset
+
+
+
+# executing holt-winters (tripe exponential smoothing)
